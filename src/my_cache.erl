9bb11de20668c51f62482ad0cee1d83c3eb8609b
Daniyl <danik133743@gmail.com>
2023-08-15 13:34:01 +0200

    Fix style code.

--- src/my_cache.erl
+++ src/my_cache.erl
@@ -6,44 +6,35 @@
 
 -export([get_current_time_in_seconds/0,get_end_time/1,get_time_in_seconds/2]).
 
+create(TableName)->
+  Res = ets:new(TableName,[set, public, named_table]),
+  {ok,Res}.
+
+insert(TableName, Key, Value, Timer)->
+  ets:insert(TableName,{Key,Value,calendar:universal_time(),get_end_time(Timer)}),
+  {ok,insert}.
+
+lookup(TableName,Key)->
+  ets:lookup(TableName,Key).
+
 lookup_all(TableName)->
   lookup_all(TableName,ets:first(TableName),[]).
 
 delete_obsolete(TableName)->
-  delete_obsolete(TableName,ets:first(TableName),[]).
+  NowSeconds = calendar:universal_time(),
+  Result = ets:select(TableName,[{{'$1','$2','$3','$4'},[{'>',{const,NowSeconds},('$4')}],['$1']}]),
+  lists:foreach(fun(Key)->ets:delete(TableName,Key) end,Result).
 
-create(TableName)->
-  ets:new(TableName,[set, public, named_table]).
-
-insert(TableName, Key, Value, Timer)->
-  ets:insert(TableName,{Key,Value,Timer,calendar:universal_time()}).
-
-lookup(TableName,Key)->
-  [{Key,_,Timer,AddDate}] = ets:lookup(TableName,Key),
-  case get_current_time_in_seconds()>get_time_in_seconds(Timer,AddDate) of
-    true->undefined;
-    false-> ets:lookup(TableName,Key)
-  end.
-
+%PRIVATE
+%=============================================================================
 lookup_all(TableName,Key,Acc)->
   case ets:next(TableName,Key) of
     '$end_of_table'-> lists:reverse([my_cache:lookup(TableName,Key)|Acc]);
     _Res -> lookup_all(TableName,_Res,[my_cache:lookup(TableName,Key)|Acc])
   end.
-
-delete_obsolete(TableName,_Key,Acc)->
-  case ets:next(TableName,_Key) of
-    '$end_of_table' -> true;
-    _Res ->
-      [{Key,Value,Timer,AddDate}] = ets:lookup(TableName,_Res),
-      CurrentDate=get_current_time_in_seconds(),
-      DataBaseDate=get_time_in_seconds(Timer,AddDate),
-      case CurrentDate>=DataBaseDate of
-        true->ets:delete(TableName,_Res),delete_obsolete(TableName,_Key,[]);
-        false->delete_obsolete(TableName,_Res,[[{Key,Value,Timer,AddDate}]|Acc])
-      end
-  end.
-
+%=============================================================================
+%TIME
+%=============================================================================
 get_end_time(Timer)->
   calendar:gregorian_seconds_to_datetime(get_current_time_in_seconds()+Timer*60).
 
@@ -52,3 +43,4 @@ get_time_in_seconds(Timer,AddDate)->
 
 get_current_time_in_seconds()->
   calendar:datetime_to_gregorian_seconds(calendar:universal_time()).
+%=============================================================================
